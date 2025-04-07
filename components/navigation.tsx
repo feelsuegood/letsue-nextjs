5a3a843c5d81f95e99443cdf5994e8b73c8b2b20
feelsuegood <suaj2021@gmail.com>
2025-04-07 22:37:50 +1000

    4.2 movie styles

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,21 +1,22 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   console.log(path);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link>
-          {path === '/' ? ' 👀' : ''}
+          {path === "/" ? " 👀" : ""}
         </li>
         <li>
           <Link href="/about-us">About Us</Link>
-          {path === '/about-us' ? ' 👀' : ''}
+          {path === "/about-us" ? " 👀" : ""}
         </li>
       </ul>
     </nav>
